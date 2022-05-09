a51b72488999c740bd129b31301d001985acbedf
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2022-05-09 17:47:04 -0400

    remove clear and update append for form

--- src/index.js
+++ src/index.js
@@ -8,25 +8,12 @@ function flagIssue(x) {if(x.pagination.items.count === 0) {alert("Anime not foun
 function adjustNull() {const AllLi = document.querySelectorAll('li');
 AllLi.forEach(z => {if (z.innerHTML === 'null') {z.innerText = "Pending"}})}
 
-/*function clear() {
-const form = document.createElement('form');
-document.querySelector('#clear').appendChild(form);
-const empty = document.createElement('input')
-form.appendChild(empty);
-empty.setAttribute("type", "submit")
-empty.setAttribute("id", "clear")
-empty.setAttribute("value", "Clear")
-empty.addEventListener('submit', () => {const ul = document.querySelectorAll('ul')
-ul.forEach(x => x.remove())})
-}*/
-
 function createSubmit() {
     const form = document.createElement('form');
     const input = document.createElement('input');
     const sub = document.createElement('input');
     document.querySelector('div').appendChild(form);
-    form.appendChild(input);
-    form.appendChild(sub);
+    form.append(input, sub);
     sub.setAttribute("type", "submit");
     input.setAttribute("type", "text");
     input.setAttribute("placeholder", "Anime");
@@ -35,7 +22,7 @@ function createSubmit() {
         if (document.querySelector('ul')) {ul = document.querySelectorAll('ul');  
         ul.forEach(x => x.remove()); 
         getAnime(input.value)
-    form.reset()} 
+        form.reset()} 
         else {getAnime(input.value);
         form.reset()}})
 }
@@ -80,7 +67,7 @@ function createLi(x) {
             if (adultAll.length > 1) {adultAll.remove()}
             else {adult.remove()}
 }
-else{createInfo(y);
+        else{createInfo(y);
 }})}
 
 
